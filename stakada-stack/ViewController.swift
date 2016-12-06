f420ada8f9de718ade2367fcc82b8061be7cc3a8
Christine Cunniff <christinecunniff@gmail.com>
2016-12-06 13:18:34 -0500

    Initial Commit

---
+++ stakada-stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stakada-stack
+//
+//  Created by Christine Cunniff on 12/6/16.
+//  Copyright © 2016 Christine Cunniff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
